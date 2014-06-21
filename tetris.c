0eed1bc308f9aaf976074ebcdfe2a7faab245fcd
Alexander Ariskin <uxcoder@yandex.com>
2014-06-21 11:55:32 +0200

    Added container and changed some logic

--- tetris.c
+++ tetris.c
@@ -7,13 +7,27 @@
 #include <GLUT/glut.h>
 #else
 #include <GL/glut.h>
+#include <GL/glu.h>
+#include <GL/gl.h>
 #endif
+
 #include <OpenGL/OpenGL.h>
 #include <stdlib.h>
 #include <sys/time.h>
 #include <stdbool.h>
 
 #include "tetris.h"
+
+
+#define LEFT 1
+#define RIGHT 2
+#define DOWN 3
+#define ROTATE 4
+
+#define INSERT 1
+#define REMOVE 0
+
+
 
 struct block {
 	int shape;
@@ -33,40 +47,43 @@ static struct {
 
 static int last_update;
 
+static int container[10*20];
+
+
+// prototypes
+void block(int action);
+void draw_cell(int x, int y, int color);
+void init(void);
 
 
 void init(void)
 {
-	glClearColor(0, 0, 0, 0); // bg colour
-	gluOrtho2D(0, CELL_SIZE * 10, CELL_SIZE * 20, 0); // viewing area
+	glClearColor(0, 0, 0, 0);
+	gluOrtho2D(0, CELL_SIZE * NCOL, CELL_SIZE * NROW, 0); // viewing area
+
+
+	game.angle = 0;
+	game.shape = 1;
+	game.x = 5;
+	game.y = 2;
+	game.tick = 1.0;
+	block(INSERT);
 }
 
-bool can_move_left(void)
-{
-	if (game.x > 1) 
-		return true;
-	
-	return false;
-}
 
-bool can_move_right(void)
+void draw_cell(int x, int y, int color)
 {
-	if (game.x < NCOL-2) 
-		return true;
-	
-	return false;
-}
+	switch (color) {
+	case 0: 
+		glColor3f(0, 0, 0);
+		break;
+	case 1: 
+		glColor3f(0, 1, 0);
+		break;	
+	default:
+		glColor3f(1, 1, 1);
+	}
 
-bool can_move_down(void)
-{
-	if (game.y < 15)
-		return true;
-	
-	return false;
-}
-
-void draw_cell(int x, int y)
-{
 	glPushMatrix();	
 	glTranslatef(x * CELL_SIZE, y * CELL_SIZE, 0);
 	glBegin(GL_QUADS);                      
@@ -78,6 +95,55 @@ void draw_cell(int x, int y)
 	glPopMatrix();
 }
 
+void display_container(void)
+{
+	for (int i = 0; i < NCOL * NROW; i++) {
+		draw_cell(i%NCOL, i/NCOL, container[i]);
+	}
+}
+
+bool check(int direction)
+{
+	return true;
+}
+
+void block(int action /* INSERT or REMOVE */) 
+{
+	int x = game.x;
+	int y = game.y;
+
+	int color = (action == INSERT) ? game.shape + 1 : 0;
+
+	for (int i = 0; i < 16; i++)
+		if (shape[game.shape][game.angle] & (0x8000 >> i))
+			container[x + y*10 + i%4 + (i/4)*10] = color;
+}
+
+
+
+
+void move(int direction)
+{
+	if (check(direction)) {
+		block(REMOVE);
+		if (direction == LEFT)
+			game.x--;
+		else if (direction == RIGHT)
+			game.x++;
+		else if (direction == DOWN)
+			game.y++;
+		else if (direction == ROTATE)
+			game.angle = game.angle < 3 ? game.angle + 1 : 0;
+
+		block(INSERT);
+	}
+}
+
+
+
+
+
+
 
 void on_idle(void)
 {
@@ -88,10 +154,9 @@ void on_idle(void)
 	int elapsed = now - last_update;
 
 	if (elapsed > game.tick * 1000) {
-		if (can_move_down()) {
-			game.y++;
-			last_update = now;
-		}
+		move(DOWN);
+		last_update = now;
+		
 		
 	}
 
@@ -110,15 +175,13 @@ void process_special_keys(int key, int x, int y)
 {
 	switch (key) {
 	case GLUT_KEY_RIGHT:
-		if (can_move_right())
-			game.x++;
+		move(RIGHT);
 		break;
 	case GLUT_KEY_LEFT:
-		if (can_move_left())
-			game.x--;
+		move(LEFT);
 		break;
 	case GLUT_KEY_UP:
-		game.angle = game.angle < 3 ? game.angle + 1 : 0;
+		move(ROTATE);
 		break;
 	case GLUT_KEY_DOWN:
 		break;
@@ -128,13 +191,16 @@ void process_special_keys(int key, int x, int y)
 
 
 
+
+
+
 void draw_shape(int type, int x, int y)
 {
-	glColor3f(1, 0, 0);
 	
-	for (int i = 0; i < 16; i++)
-		if (shape[type][game.angle] & (0x8000 >> i))
-			draw_cell(x + i%4 - 2, y + i/4 + 1);	
+	
+	// for (int i = 0; i < 16; i++)
+	// 	if (shape[type][game.angle] & (0x8000 >> i))
+	// 		draw_cell(x + i%4 - 2, y + i/4 + 1);	
 
 }
 
@@ -142,17 +208,16 @@ void render(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	draw_shape(game.shape, game.x, game.y);
+	// draw_shape(game.shape, game.x, game.y);
+
+	
+	display_container();
 	glutSwapBuffers();
 }
 
 int main(int argc, char **argv)
 {
-	game.angle = 0;
-	game.shape = 1;
-	game.x = 5;
-	game.y = 2;
-	game.tick = 1.0;
+	
 
 	// struct timeval t;
 	// gettimeofday(&t, NULL);
