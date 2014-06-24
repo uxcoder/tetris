40fda30e06eef9e22567348b6224fbb9a4cf3e96
Alexander Ariskin <uxcoder@yandex.com>
2014-06-24 21:14:42 +0200

    Small changes and fixes

--- tetris.c
+++ tetris.c
@@ -20,34 +20,19 @@
 #include "tetris.h"
 
 
-#define LEFT 1
-#define RIGHT 2
-#define DOWN 3
-#define ROTATE 4
-
-#define REMOVE 0
-#define INSERT 1
-
-#define BORDER 8
-
-#define WIDTH (NCOL + 2)
-#define HEIGHT (NROW + 1)
-
-#define W (CELL_SIZE*(NCOL+10))
-#define H (CELL_SIZE*(NROW+4))
-
-static struct {
+static struct tetrablock {
 	int type;
 	int x;
 	int y;
 	int rot;
-} current;
+};
 
 static struct {
 	bool running;
 	int tick;
 	int last_update;
-	int container[WIDTH * HEIGHT];
+	int container[NCOLB * NROWB];
+	struct tetrablock block;
 } game;
 
 
@@ -59,46 +44,52 @@ void draw_cell(int x, int y, int i);
 void init(void);
 bool check(int x, int y, int angle);
 void spawn(void);
-void shift(int row);
+void drop(int row);
 
 
 void init(void)
 {
 	srandomdev(); 
 	
-	glClearColor(0.0, 0.0, 0.0, 0.0);
-	gluOrtho2D(0, W, H, 0);
+	glClearColor(0, 0, 0, 0);
+	gluOrtho2D(0, WIDTH, HEIGHT, 0);
 
 	game.running = true;
 	game.last_update = glutGet(GLUT_ELAPSED_TIME);
 
-
-	for (int i = 0; i < NCOL; i++) {
-		game.container[WIDTH*(HEIGHT-1)+i+1] = BORDER;
-	}
+	for (int i = 0; i < NCOL; i++)
+		game.container[NCOLB * (NROWB - 1) + i + 1] = 1;
 
 	for (int i = 0; i < NROW; i++) {
-		game.container[WIDTH*i] = BORDER;
-		game.container[WIDTH*i+NCOL+1] = BORDER;
+		game.container[NCOLB * i] = 1;
+		game.container[NCOLB * i + NCOL + 1] = 1;
 	}
 
 	spawn();
 
 }
 
+void exitgame(void)
+{
+	free(game.container);
+}
+
 
 void spawn(void)
 {
-	current.type = random() % 7;
-	current.x = 4;
-	current.y = -1;
-	current.rot = 0;
-	game.tick = 500;
+	
+	game.block.type = random() % 7;
+	game.block.x 	= 4;
+	game.block.y 	= -1;
+	game.block.rot 	= 0;
+	game.tick 	= 500;
 
-	if (check(current.x, current.y, current.rot))
+	if (check(game.block.x, game.block.y, game.block.rot))
 		block(INSERT);
-	else
+	else {
 		game.running = false;
+		exitgame();
+	}
 	
 }
 
@@ -106,39 +97,48 @@ void spawn(void)
 bool full(int row)
 {
 	for (int i = 0; i < NCOL; i++) {
-		if (!game.container[1 + i + row * WIDTH])
+		if (!game.container[1 + i + row * NCOLB])
 			return false;
 	}
 	return true;
 }
 
 
-void shift(int row)
+void drop(int row)
 {
 	for (int i = 0; i < NCOL; i++)
-		game.container[i+1 + row * WIDTH] = game.container[i+1+(row-1)*WIDTH];
+		game.container[1 + i + row * NCOLB] = 
+			game.container[1 + i + (row - 1) * NCOLB];
 }
 
 void draw_grid(void)
 {
 	glColor3f(0.1, 0.1, 0.1);
-	for (int x = 0; x < W; x++)
-		for (int y = 0; y < H; y++) {
-			glBegin(GL_LINES);
-			glVertex2f(x*CELL_SIZE, 0);
-			glVertex2f(x*CELL_SIZE, H*CELL_SIZE);
-			glVertex2f(0, y*CELL_SIZE);
-			glVertex2f(W*CELL_SIZE, y*CELL_SIZE);
-			glEnd();
-		}
+	int xoffset = 5 * CELL_SIZE;
+	int yoffset = 1 * CELL_SIZE;
+	glPushMatrix();	
+	glTranslatef(xoffset, yoffset, 0);
+	for (int x = 0; x <= NCOL; x++) {
+		glBegin(GL_LINES);
+		glVertex2f(x * CELL_SIZE, 0);
+		glVertex2f(x * CELL_SIZE, NROW * CELL_SIZE);
+		glEnd();
+	}
+	for (int y = 0; y <= NROW; y++) {
+		glBegin(GL_LINES);
+		glVertex2f(0, y * CELL_SIZE);
+		glVertex2f(NCOL * CELL_SIZE, y * CELL_SIZE);
+		glEnd();
+	}
+	glPopMatrix();
 }
 
 
 void draw_cell(int x, int y, int color)
 {
 	unsigned long palette[] = {
-		0x000000, 0xFF0000, 0xFFFF00, 0xFFFFFF, 0x00FF00, 
-		0x0000FF, 0x00FFFF, 0xFF00FF, 0x222222
+		0x000000, 0xFF0000, 0xFFFF00, 0xFFFFFF, 
+		0x00FF00, 0x0000FF, 0x00FFFF, 0xFF00FF
 	};
 	
 	int xoffset = (x+4) * CELL_SIZE;
@@ -150,28 +150,26 @@ void draw_cell(int x, int y, int color)
 
 	glPushMatrix();	
 	glTranslatef(xoffset, yoffset, 0);
-	glBegin(GL_QUADS);                      
-	glVertex2f(0, 0);
-	glVertex2f(0, CELL_SIZE);
-	glVertex2f(CELL_SIZE, CELL_SIZE);
-	glVertex2f(CELL_SIZE, 0);
-    	glEnd(); 	
+	glRectf(0.0f, 0.0f, CELL_SIZE, CELL_SIZE);
 	glPopMatrix();
 }
 
 
 void display_container(void)
 {
-	for (int i = 0; i < WIDTH * HEIGHT; i++)
-		draw_cell(i%WIDTH, i/WIDTH, game.container[i]);
+	for (int i = 0; i < NCOL * NROW; i++) {
+		if (game.container[1 + i + 2 * (i / NCOL)])
+			draw_cell(i % NCOL + 1, i / NCOL, 
+				game.container[1 + i + 2 * (i / NCOL)]);
+	}
 }
 
 
 bool check(int x, int y, int rot)
 {
 	for (int i = 0; i < 16; i++)
-		if ((tetra_block[current.type][rot] & (0x8000 >> i))
-			&& game.container[x + i%4 + WIDTH * (y + i/4)])
+		if ((tblock[game.block.type][rot] & (0x8000 >> i))
+			&& game.container[x + i % 4 + NCOLB * (y + i / 4)])
 			return false;
 	return true;
 }
@@ -180,9 +178,10 @@ bool check(int x, int y, int rot)
 void block(int action /* INSERT or REMOVE */) 
 {
 	for (int i = 0; i < 16; i++)
-		if (tetra_block[current.type][current.rot] & (0x8000 >> i))
-			game.container[current.x + i%4 + WIDTH * (current.y + i/4)] = 
-				(action == INSERT) ? current.type + 1 : 0;
+		if (tblock[game.block.type][game.block.rot] & (0x8000 >> i))
+			game.container[game.block.x + i % 4 + 
+				NCOLB * (game.block.y + i / 4)] = 
+				(action == INSERT) ? game.block.type + 1 : 0;
 }
 
 
@@ -190,31 +189,30 @@ bool move(int direction)
 {
 	bool res = true;
 
-	// TODO use switch case block
 	block(REMOVE);
 	if (direction == LEFT) {
-		if (check(current.x-1, current.y, current.rot)) {
-			current.x--;
+		if (check(game.block.x-1, game.block.y, game.block.rot)) {
+			game.block.x--;
 		}
 		
 	}
 	else if (direction == RIGHT) {
-		if (check(current.x+1, current.y, current.rot)) {
-			current.x++;
+		if (check(game.block.x+1, game.block.y, game.block.rot)) {
+			game.block.x++;
 		}
 	}
 
 	else if (direction == DOWN) {
-		if (check(current.x, current.y+1, current.rot)) {
-			current.y++;
+		if (check(game.block.x, game.block.y+1, game.block.rot)) {
+			game.block.y++;
 		} else {
 			res = false;	
 		}
 	}
 	else if (direction == ROTATE) {
-		int rot = current.rot < 3 ? current.rot + 1 : 0;
-		if (check(current.x, current.y, rot)) {
-			current.rot = rot;
+		int rot = game.block.rot < 3 ? game.block.rot + 1 : 0;
+		if (check(game.block.x, game.block.y, rot)) {
+			game.block.rot = rot;
 		}
 	}	
 	
@@ -223,18 +221,12 @@ bool move(int direction)
 }
 
 
-int check_lines(void)
+void check_lines(void)
 {
-	int lines = 0;
-
-	for (int row = 0; row <= NROW - 1; row++) {
-		if (full(row)) {
-			lines++;
+	for (int row = 0; row < NROW; row++)
+		if (full(row))
 			for (int i = row; i > 1; i--)
-				shift(i);
-		}
-	}
-	return lines;
+				drop(i);
 }
 
 
@@ -287,7 +279,7 @@ void render(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	display_container();
-	// draw_grid();
+	draw_grid();
 	glutSwapBuffers();
 }
 
@@ -295,7 +287,7 @@ void render(void)
 int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
-	glutInitWindowSize(W, H);
+	glutInitWindowSize(WIDTH, HEIGHT);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutCreateWindow("Tetris");
 	glutDisplayFunc(render);
@@ -305,7 +297,7 @@ int main(int argc, char **argv)
 	init();
 	glutMainLoop();
 	
-	return 0;
+	return EXIT_SUCCESS;
 }
 
 
--- tetris.h
+++ tetris.h
@@ -1,13 +1,24 @@
 #ifndef __TETRIS_H
 #define __TETRIS_H
 
-#define NCOL 10
-#define NROW 20
-#define CELL_SIZE 20
-#define NROTATIONS 4
-#define NSHAPES 7
+#define NCOL 		10
+#define NROW 		20
+#define NCOLB 		(NCOL + 2) // Add borders left and right
+#define NROWB 		(NROW + 1) // Additinal border at the bottom
+#define CELL_SIZE 	20
+#define WIDTH		(CELL_SIZE * (NCOL + 10))
+#define HEIGHT	 	(CELL_SIZE * (NROW + 4))
+	
+#define LEFT 		1
+#define RIGHT 		2
+#define DOWN 		3
+#define ROTATE 		4
 
-int tetra_block[7][4] = {
+#define REMOVE 		0
+#define INSERT 		1
+
+
+int tblock[7][4] = {
 	{0x0F00, 0x2222, 0x0F00, 0x2222}, /* I */
 	{0x0710, 0x3220, 0x0470, 0x2260}, /* J */
 	{0x0740, 0x2230, 0x1700, 0x6220}, /* L */
